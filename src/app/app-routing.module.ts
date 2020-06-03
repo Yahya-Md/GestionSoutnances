4eec88e32d499237d0e63ce2aa83961962b583a7
Yahya-Md <Yahya-Md@users.noreply.github.com>
2020-06-03 22:00:06 +0100

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AcceuilComponent} from './website/acceuil/acceuil.component';
+import {InsciptionComponent} from './website/insciption/insciption.component';
+import {LoginComponent} from './website/login/login.component';
+import {NotfoundComponent} from './website/notfound/notfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    redirectTo : 'acceuil',
+    pathMatch : 'full'
+  },
+  {
+    path : 'acceuil',
+    component : AcceuilComponent
+  },
+  {
+    path : 'inscription',
+    component : InsciptionComponent
+  },
+  {
+    path : 'login',
+    component : LoginComponent
+  },
+  {
+    path : '**',
+    component : NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WebsiteComponent } from './website/website.component';
+import { AcceuilComponent } from './website/acceuil/acceuil.component';
+import { LoginComponent } from './website/login/login.component';
+import { InsciptionComponent } from './website/insciption/insciption.component';
+import { NotfoundComponent } from './website/notfound/notfound.component';
+import { HeaderNavComponent } from './website/navigation/header-nav/header-nav.component';
+import { FooterNavComponent } from './website/navigation/footer-nav/footer-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebsiteComponent,
+    AcceuilComponent,
+    LoginComponent,
+    InsciptionComponent,
+    NotfoundComponent,
+    HeaderNavComponent,
+    FooterNavComponent
   ],
   imports: [
     BrowserModule,
